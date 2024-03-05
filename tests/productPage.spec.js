7c2f4a74b6d7b25799ad741c492bf5d86fddb84b
Wiktor <frelich.wiktor@gmail.com>
2024-03-05 22:40:41 +0100

    Fix.

--- tests/productPage.spec.js
+++ tests/productPage.spec.js
@@ -1,46 +1,43 @@
-import { test, expect } from '@playwright/test';
-import { productPage } from '../pages/productPage.js';
+import { test, expect } from "@playwright/test";
+import { productPage } from "../pages/productPage.js";
 
-test.beforeEach(
-  'Setup - entering a page',
-  async ({ page }) => {
-    const Product = new productPage(page);
-    await Product.goToPage();
-    await Product.acceptCookies()
-  },
-);
-
-test('Product name is correct', async ({ page }) => {
+test.beforeEach("Setup - entering a page", async ({ page }) => {
   const Product = new productPage(page);
-  await Product.productNameIsCorrect('Kask Cross Airoh Twist 2.0 Czarny');
+  await Product.goToPage();
+  await Product.acceptCookies();
 });
 
-test('Page header is visible', async ({ page }) => {
+test("Product name is correct", async ({ page }) => {
+  const Product = new productPage(page);
+  await Product.productNameIsCorrect("Kask Cross Airoh Twist 2.0 Czarny");
+});
+
+test("Page header is visible", async ({ page }) => {
   const Product = new productPage(page);
   await Product.pageHeaderIsVisible();
 });
 
-test('Page header menu is visible', async ({ page }) => {
-    const Product = new productPage(page);
-    await Product.pageHeaderMenuIsVisible();
-  });
+test("Page header menu is visible", async ({ page }) => {
+  const Product = new productPage(page);
+  await Product.pageHeaderMenuIsVisible();
+});
 
-test('Page meta links are visible', async ({ page }) => {
-    const Product = new productPage(page);
-    await Product.pageHeaderMetaLinksAreVisible();
-  });
+test("Page meta links are visible", async ({ page }) => {
+  const Product = new productPage(page);
+  await Product.pageHeaderMetaLinksAreVisible();
+});
 
-  test('Product photo is visible', async ({ page }) => {
-    const Product = new productPage(page);
-    await Product.productPhotoIsVisible();
-  });
+test("Product photo is visible", async ({ page }) => {
+  const Product = new productPage(page);
+  await Product.productPhotoIsVisible();
+});
 
-  test('Product price is visible', async ({ page }) => {
-    const Product = new productPage(page);
-    await Product.productPriceIsVisible();
-  });
+test("Product price is visible", async ({ page }) => {
+  const Product = new productPage(page);
+  await Product.productPriceIsVisible();
+});
 
-  test('Compare a product page with a screenshot', async ({ page }) => {
-    const Product = new productPage(page);
-    await expect(page).toHaveScreenshot()
-  });
+test("Compare a product page with a screenshot", async ({ page }) => {
+  const Product = new productPage(page);
+  await expect(page).toHaveScreenshot("productPage.png");
+});
